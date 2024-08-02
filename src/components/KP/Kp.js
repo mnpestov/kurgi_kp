0151861c34bf59b81b5e62a625966125acea369b
Mikhail Pestov <mnpestov@gmail.com>
2024-08-02 18:13:12 +0300

    Верстка

--- src/components/KP/Kp.js
+++ src/components/KP/Kp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import './Kp.css';
 import logo from '../../images/logo.png'
 
-function Kp({startEvent,endEvent,startTime,endTime,eventPlace,countOfPerson,}) {
+function Kp({ startEvent, endEvent, startTime, endTime, eventPlace, countOfPerson, }) {
     // const current = new Date();
     // const options = {
     //     hour: 'numeric', minute: 'numeric', second: 'numeric',
@@ -21,30 +21,53 @@ function Kp({startEvent,endEvent,startTime,endTime,eventPlace,countOfPerson,}) {
     // const [countOfPerson, setCountOfPerson] = useState("")
 
     return (
-                <div className="list">
-                    <div className="top">
-                        <div className="left">
-                            <div className="list__logo-container">
-                                <img className="list__logo" src={logo} alt="logo" />
-                            </div>
-                            <div className="list__title-container">
-                                <h2 className="list__title">Фудтраки StreetFOOD</h2>
-                            </div>
-                            <div className="list__subtitle-container">
-                                <p className="list__subtitle list__subtitle_place">{`${startEvent}-${endEvent} место: ${eventPlace};`}</p>
-                                <p className="list__subtitle list__subtitle_person">{`кол-во персон: ${countOfPerson}`}</p>
-                                <p className="list__subtitle list__subtitle_time">{`время мероприятия: ${startTime.slice(-6)} (${startEvent.slice(0,5)}) -${endTime.slice(-6)} (${endEvent.slice(0,5)}) `}</p>
-                            </div>
-                        </div>
-                        <div className="right">
-                            <p className="table__column">Количество, шт</p>
-                            <p className="table__column">Стоимость</p>
-                            <p className="table__column">Цена, руб</p>
-                        </div>
-                    </div>
-                    <div className="middle"></div>
-                    <div className="bottom"></div>
+        <div className="list">
+            <img className="list__logo" src={logo} alt="logo" />
+            <h2 className="list__title">Фудтраки StreetFOOD</h2>
+            <table className="list__table">
+                <tr>
+                    <th className="list__subtitle-container">
+                        <p className="list__subtitle list__subtitle_place">{`${startEvent}-${endEvent} место: ${eventPlace};`}</p>
+                        <p className="list__subtitle list__subtitle_person">{`кол-во персон: ${countOfPerson}`}</p>
+                        <p className="list__subtitle list__subtitle_time">{`время мероприятия: ${startTime.slice(-6)} (${startEvent.slice(0, 5)}) -${endTime.slice(-6)} (${endEvent.slice(0, 5)}) `}</p>
+                    </th>
+
+                    <th className="table__title">
+                        <p className="table__column">Количество, шт</p>
+                    </th>
+
+                    <th className="table__title">
+                        <p className="table__column">Стоимость</p>
+                    </th>
+
+                    <th className="table__title">
+                        <p className="table__column">Цена, руб</p>
+                    </th>
+                </tr>
+            </table>
+            <div className="top">
+                <div className="left">
+                    {/* <div className="list__logo-container">
+                        <img className="list__logo" src={logo} alt="logo" />
+                    </div> */}
+                    {/* <div className="list__title-container">
+                        <h2 className="list__title">Фудтраки StreetFOOD</h2>
+                    </div> */}
+                    {/* <div className="list__subtitle-container">
+                        <p className="list__subtitle list__subtitle_place">{`${startEvent}-${endEvent} место: ${eventPlace};`}</p>
+                        <p className="list__subtitle list__subtitle_person">{`кол-во персон: ${countOfPerson}`}</p>
+                        <p className="list__subtitle list__subtitle_time">{`время мероприятия: ${startTime.slice(-6)} (${startEvent.slice(0, 5)}) -${endTime.slice(-6)} (${endEvent.slice(0, 5)}) `}</p>
+                    </div> */}
                 </div>
+                <div className="right">
+                    <p className="table__column">Количество, шт</p>
+                    <p className="table__column">Стоимость</p>
+                    <p className="table__column">Цена, руб</p>
+                </div>
+            </div>
+            <div className="middle"></div>
+            <div className="bottom"></div>
+        </div>
     );
 
 }
