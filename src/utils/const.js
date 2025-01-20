09abb05afca594a6c8328c805d77472e2e81b0db
Mikhail Pestov <mnpestov@gmail.com>
2025-01-20 17:21:17 +0300

    убрать предзаполненные данные

--- src/utils/const.js
+++ src/utils/const.js
@@ -26,114 +26,114 @@ export const rows = [
 ]
 
 export const lists = [
-    {
-        id: 1,
-        rows: [
-            {
-                countOfProduct: 400,
-                priceOfProduct: 450,
-                product: "Бургеры",
-                composition: "( BBQ / Спайси )",
-                productWeight: 270,
-                typeOfProduct: "eat",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Картофель ФРИ",
-                composition: "(Соус сырный / кетчуп)",
-                productWeight: 110,
-                typeOfProduct: "eat",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Мороженое в рожках",
-                composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
-                productWeight: 60,
-                typeOfProduct: "eat",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Чай",
-                composition: "(черный / зеленый)",
-                productWeight: 110,
-                typeOfProduct: "drink",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Мороженое в рожках",
-                composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
-                productWeight: 60,
-                typeOfProduct: "eat",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 450,
-                product: "Кофе",
-                composition: "( американо / капучино )",
-                productWeight: 270,
-                typeOfProduct: "drink",
-            },
-            {
-                countOfProduct: 4,
-                priceOfProduct: 10000,
-                product: "Повар",
-                composition: "(по согласованному таймингу)",
-                typeOfProduct: "organisation",
-            },
-            {
-                countOfProduct: 1,
-                priceOfProduct: 20000,
-                product: "Фудтрак",
-                composition: "5500х2400х3200",
-                typeOfProduct: "organisation",
-            },
-        ]
-    },
-    {
-        id: 2,
-        rows: [
-            {
-                countOfProduct: 400,
-                priceOfProduct: 450,
-                product: "Кофе",
-                composition: "( американо / капучино )",
-                productWeight: 270,
-                typeOfProduct: "drink",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Чай",
-                composition: "(черный / зеленый)",
-                productWeight: 110,
-                typeOfProduct: "drink",
-            },
-            {
-                countOfProduct: 400,
-                priceOfProduct: 200,
-                product: "Квас",
-                composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
-                productWeight: 60,
-                typeOfProduct: "drink",
-            },
-            {
-                countOfProduct: 4,
-                priceOfProduct: 10000,
-                product: "Повар",
-                composition: "(по согласованному таймингу)",
-                typeOfProduct: "organisation",
-            },
-            {
-                countOfProduct: 1,
-                priceOfProduct: 20000,
-                product: "Фудтрак",
-                composition: "5500х2400х3200",
-                typeOfProduct: "organisation",
-            },
-        ]
-    }
+    // {
+    //     id: 1,
+    //     rows: [
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 450,
+    //             product: "Бургеры",
+    //             composition: "( BBQ / Спайси )",
+    //             productWeight: 270,
+    //             typeOfProduct: "eat",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Картофель ФРИ",
+    //             composition: "(Соус сырный / кетчуп)",
+    //             productWeight: 110,
+    //             typeOfProduct: "eat",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Мороженое в рожках",
+    //             composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
+    //             productWeight: 60,
+    //             typeOfProduct: "eat",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Чай",
+    //             composition: "(черный / зеленый)",
+    //             productWeight: 110,
+    //             typeOfProduct: "drink",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Мороженое в рожках",
+    //             composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
+    //             productWeight: 60,
+    //             typeOfProduct: "eat",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 450,
+    //             product: "Кофе",
+    //             composition: "( американо / капучино )",
+    //             productWeight: 270,
+    //             typeOfProduct: "drink",
+    //         },
+    //         {
+    //             countOfProduct: 4,
+    //             priceOfProduct: 10000,
+    //             product: "Повар",
+    //             composition: "(по согласованному таймингу)",
+    //             typeOfProduct: "organisation",
+    //         },
+    //         {
+    //             countOfProduct: 1,
+    //             priceOfProduct: 20000,
+    //             product: "Фудтрак",
+    //             composition: "5500х2400х3200",
+    //             typeOfProduct: "organisation",
+    //         },
+    //     ]
+    // },
+    // {
+    //     id: 2,
+    //     rows: [
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 450,
+    //             product: "Кофе",
+    //             composition: "( американо / капучино )",
+    //             productWeight: 270,
+    //             typeOfProduct: "drink",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Чай",
+    //             composition: "(черный / зеленый)",
+    //             productWeight: 110,
+    //             typeOfProduct: "drink",
+    //         },
+    //         {
+    //             countOfProduct: 400,
+    //             priceOfProduct: 200,
+    //             product: "Квас",
+    //             composition: "( клубничное / шоколадное / ванильное / фисташковое / баблгам / черешня)",
+    //             productWeight: 60,
+    //             typeOfProduct: "drink",
+    //         },
+    //         {
+    //             countOfProduct: 4,
+    //             priceOfProduct: 10000,
+    //             product: "Повар",
+    //             composition: "(по согласованному таймингу)",
+    //             typeOfProduct: "organisation",
+    //         },
+    //         {
+    //             countOfProduct: 1,
+    //             priceOfProduct: 20000,
+    //             product: "Фудтрак",
+    //             composition: "5500х2400х3200",
+    //             typeOfProduct: "organisation",
+    //         },
+    //     ]
+    // }
 ]
