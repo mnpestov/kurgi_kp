0f3171cd5ce609ce7488806f3fd07cdd8e371f1f
Mikhail Pestov <mnpestov@gmail.com>
2025-01-20 16:19:12 +0300

    hotfix

--- src/components/KP/Kp.js
+++ src/components/KP/Kp.js
@@ -51,8 +51,8 @@ function Kp({ startEvent, endEvent, startTime, endTime, eventPlace, countOfPerso
                             {/* <p className="list__logistic-cost">{isWithinMkad
                                 ? `Логистика в пределах МКАД + монтаж / демонтаж: ${GetPrice(logisticsCost)}`
                                 : `Логистика за пределами МКАД + монтаж / демонтаж: ${GetPrice(logisticsCost)}`}</p> */}
-                            <p className="list__totla-cost">{`Итоговая сумма: ${GetPrice(parseInt(totalCost) + parseInt(logisticsCost))}`}</p>
-                            <p className="list__totla-cost">{`Итого по безналичному расчёту: ${GetPrice(Math.round((parseInt(totalCost) + parseInt(logisticsCost)) * 1.07))}`}</p>
+                            <p className="list__totla-cost">{`Итоговая сумма: ${GetPrice(parseInt(totalCost))}`}</p>
+                            <p className="list__totla-cost">{`Итого по безналичному расчёту: ${GetPrice(Math.round((parseInt(totalCost)) * 1.07))}`}</p>
 
                         </div>
                     </div>
